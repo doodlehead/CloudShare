6c2596929a7d027a6b2f081e579abe2b40b45a4f
Frank Yu <frankyu1999@gmail.com>
2016-10-20 18:45:11 +0000

    Correct the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,2 @@
 class User < ApplicationRecord
-    #Force all emails to be lowercase
-    before_save { self.email = email.downcase }
-    
-    #Validate name and email
-    validates :name, presence: true
-    validates :email, presence: true
-    
-   # 
-    has_secure_password
-    validates :password
 end
--- db/migrate/20161020184311_create_users.rb
+++ db/migrate/20161020184311_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.String :name
-      t.String :email
+      t.string :name
+      t.string :email
 
       t.timestamps
     end
