25ec0888366044a07217b96dbfc10ea8356d220a
Frank Yu <frankyu1999@gmail.com>
2016-10-21 16:42:38 +0000

    Add validations and password functionality for the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    #Force all emails to be lowercase
+    before_save { self.email = email.downcase }
+    
+    #Validate name and email
+    validates :name, presence: true, length: { maximum: 30 }
+    #Regex that identifies a valid email string
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+    
+    #Adds password functinality(BCrypt gem)
+    has_secure_password
+    validates :password, length: { minimum: 6}, presence: true
 end
