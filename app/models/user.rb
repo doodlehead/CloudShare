dfdd98be569b603af3a800468299a4300cc51e62
Frank Yu <frankyu1999@gmail.com>
2016-10-24 18:10:59 +0000

    Fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,16 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { maximum: 30 }
     #Regex that identifies a valid email string
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
     
     #Adds password functinality(BCrypt gem)
     has_secure_password
     validates :password, length: { minimum: 6}, presence: true
+    
+    #Password digest method
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
 end
