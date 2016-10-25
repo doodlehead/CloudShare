85860f0eceea0953b73dfbcafe033b71011ec063
Frank Yu <frankyu1999@gmail.com>
2016-10-25 03:49:15 +0000

    Fill in views for home and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
+  
   def new
-    
-    @User = User.new
+    #Create empty user
+    @user = User.new
   end
 
   def show
   end
 
   def create
+    #@user = User.new(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
   end
 end
