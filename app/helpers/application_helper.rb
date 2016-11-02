8b609ba03e6751a56f05cc697bb691d89daade48
William Lu <william@tamedfox.ca>
2016-11-01 21:30:51 -0400

    Fixed base title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def full_title(page_title = ' ')
-        base_title = "Ruby on Rails tutorial Sample App"
+        base_title = "CloudShare"
         
         if page_title.empty?
             base_title
