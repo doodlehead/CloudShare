d14290bd38628509a8123774053e9f863a42870c
Frank Yu <frankyu1999@gmail.com>
2016-12-22 19:39:38 +0000

    Fix storage calculate bug

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -1,5 +1,5 @@
 class AssetsController < ApplicationController
-  before_action :logged_in_user, only: [:set_asset, :index, :get, :show, :new, :create,:update, :destroy]
+  before_action :logged_in_user, only: [:set_asset, :index, :edit, :get, :show, :new, :create,:update, :destroy]
   before_action :set_asset, only: [:show, :get, :edit, :update, :destroy]
   before_action :admin_user, only: [:update, :edit]
   before_action :has_storage, only: [:create]
@@ -35,15 +35,18 @@ class AssetsController < ApplicationController
   def create
     @asset = current_user.assets.new(asset_params)
     
-    #byebug
-    
-    respond_to do |format|
-      if @asset.save
-        format.html { redirect_to @asset, notice: 'Asset was successfully created.' }
-        format.json { render :show, status: :created, location: @asset }
-      else
-        format.html { render :new }
-        format.json { render json: @asset.errors, status: :unprocessable_entity }
+    if(@asset.asset.size + calculate_storage(current_user) > (1024*1024*5))
+      flash[:danger] = "You do not have enough storage to upload this file! Please delete some files to free up space."
+      redirect_to assets_path
+    else
+      respond_to do |format|
+        if @asset.save
+          format.html { redirect_to @asset, notice: 'Asset was successfully created.' }
+          format.json { render :show, status: :created, location: @asset }
+        else
+          format.html { render :new }
+          format.json { render json: @asset.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -82,7 +85,7 @@ class AssetsController < ApplicationController
       end
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    # Never trust parameters from the s.cary internet, only allow the white list through.
     def asset_params
       #byebug
       params.require(:asset).permit(:user_id, :asset)
